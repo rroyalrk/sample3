708b9e8962ba4828bbf172844694e64985d3f401
KATTA ROHITH KUMAR <46136394+rroyalrk@users.noreply.github.com>
2024-09-13 13:34:41 -0700

    Update script.js

--- experience/script.js
+++ experience/script.js
@@ -66,10 +66,10 @@ document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
         document.title = "Experience | Portfolio Jigar Sable";
-        $("#favicon").attr("href","/assets/images/favicon.png");
+        $("#favicon").attr("href","/assets/images/ppb.png");
     }
     else {
         document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href","/assets/images/favhand.png");
+        $("#favicon").attr("href","/assets/images/ppb.png");
     }
 });
